623b3984987ad6b6f3e8c1be1a708bf775a5c19e
Son Hong Su <son.ho463@gmail.com>
2022-04-30 23:47:10 +0900

    ハンバーガー見た目まで

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,3 +7,31 @@ const month = document.querySelector('#js_month');
 const day = document.querySelector('#js_day');
 month.textContent = mo+1;
 day.textContent = da
+
+
+// footer-cvがでてきたらcv-button非表示
+$(window).scroll(function () {
+  $('.js-footer').each(function () {
+    var hit = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var wHeight = $(window).height();
+    var customTop = 100;
+    if (typeof $(this).data('js-footer') !== 'undefined') {
+      // data-effect="任意の値" が設定されている場合
+      customTop = $(this).data('js-footer');
+    }
+    if (hit + customTop < wHeight + scroll) {
+      $('.js-cv-button').addClass("is-hidden");
+    } else {
+      $('.js-cv-button').removeClass("is-hidden");
+		}
+  });
+});
+
+const ham = $('.js-hamburger-btn');
+const nav = $('.js-g-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  // ham.toggleClass('is-active'); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass('is-active'); // ナビゲーションメニューにactiveクラスを付け外し
+
+});
